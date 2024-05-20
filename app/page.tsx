0feb365170ccaf9f26d350c7d071e65e11ff9ffa
ksingletary <keithsingletary@gmail.com>
2024-05-20 16:37:22 -0400

    navbar animation functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-import { SiOpensea} from "react-icons/si";
-import { TbBrandCoinbase } from "react-icons/tb";
 
 export default function Home() {
   return (
     <>
-      <div className="w-screen h-auto relative flex flex-row-reverse items-center bg-blue-200 min-h-[750px]">
-        <Image src="/redpanda.png" width={700} height={700} alt="red panda pic" className=""/>
-        <div className=" ml-44 text-blackLight text-center space-y-6">
+      <div className="background-image w-screen h-auto relative flex flex-row-reverse items-center bg-blue-200 min-h-[750px]">
+        <Image src="/redpanda.png" width={700} height={700} alt="red panda pic" className="" priority/>
+        <div className=" ml-44 text-white text-center space-y-6">
           <h1 className="text-7xl font-apercuBold">
             Beat Up But Okay
           </h1>
@@ -17,13 +15,13 @@ export default function Home() {
           </p>
           <div className="flex justify-center space-x-6">
             <Link href="#" className="text-blue-500">
-              <SiOpensea className="w-8 h-8" />
+              <Image src="/opensea.svg" alt="opensea logo" width={100} height={100} className="w-8 h-8" />
             </Link>
             <Link href="#" className=" text-indigo-700">
               <Image src="/solana-sol-logo.svg" alt="solana logo" width={100} height={100} className="w-8 h-8"/>
             </Link>
             <Link href="#">
-              <TbBrandCoinbase className="w-8 h-8 text-blue-700" />
+              <Image src="/ME.png" alt="magic eden logo" width={100} height={100} className="w-8 h-8"/>
             </Link>
           </div>
         </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,44 +6,95 @@ import { useState } from "react";
 import Image from "next/image";
 
 export function Navbar() {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false); //indicated if the closing animation is in progress
 
   const handleToggle = () => {
-    setOpen(!open);
+    if (open) {
+      setIsAnimating(true);
+      setTimeout(() => {
+        setOpen(false);
+        setIsAnimating(false);
+      }, 200);
+    } else {
+      setOpen(true);
+    }
+  };
+  
+  // this is called when the mouse leaves the expanded navbar.
+  // checks if mouse has left both the original navbar and the expanded navbar areas by using e.relatedTarget and closest method.
+  const handleMouseLeave = (e: React.MouseEvent) => {
+    const target = e.relatedTarget as HTMLElement;
+    if (target && !target.closest('.navbar') && !target.closest('.expanded-navbar')) {
+      setIsAnimating(true);
+      setTimeout(() => {
+        setOpen(false);
+        setIsAnimating(false);
+      }, 200); 
+    }
   };
 
   return (
-    <nav className="bg-blackLight fixed top-0 left-0 h-full w-36 p-4">
+    <nav className="navbar bg-blackLight fixed top-0 left-0 h-full w-36 p-4">
       <div className="flex flex-col items-start h-full relative">
         <div className="text-white font-bold">
           <Link href="/">
-            <Image src="/logobub.png" alt="bublogo" width={200} height={200} className="" />
+            <Image
+              src="/logobub.png"
+              alt="bublogo"
+              width={200}
+              height={200}
+              className=""
+            />
           </Link>
         </div>
-       
+
         <button onClick={handleToggle} className="mt-auto text-white">
           <FaBarsStaggered className="w-8 h-8 ml-6" />
         </button>
-       
-        {open && (
-          <div className="fixed left-36 outline outline-black top-0 h-full w-[calc(33.000%-60px)] p-0 bg-blackLight font-CaeciliaSemi text-white bg-opacity-75 backdrop-blur-md">
-            <div className="flex flex-col justify-center h-full text-2xl -space-y-5 -mt-14 ">
-              <Link href="/about" className="hover:bg-orange-500 block px-4 py-8 w-full text-right">
+
+        {(open || isAnimating) && (
+          <div
+            onMouseLeave={handleMouseLeave}
+            className={`expanded-navbar fixed left-36 outline outline-black top-0 h-full w-[calc(33.000%-60px)] p-0 bg-blackLight font-CaeciliaSemi text-white bg-opacity-75 backdrop-blur-md ${
+              open && !isAnimating ? "navbar-open" : "navbar-close"
+            }`}
+          >
+            <div className="flex flex-col justify-center h-full text-2xl -space-y-5 -mt-14">
+              <Link
+                href="/about"
+                className="hover:bg-orange-500 block px-4 py-8 w-full text-right"
+              >
                 About
               </Link>
-              <Link href="/" className="hover:bg-orange-500 block px-4 py-8 w-full text-right">
-                Archives
+              <Link
+                href="/"
+                className="hover:bg-orange-500 block px-4 py-8 w-full text-right"
+              >
+                The Team
               </Link>
-              <Link href="/" className="hover:bg-orange-500 block px-4 py-8 w-full text-right">
+              <Link
+                href="/"
+                className="hover:bg-orange-500 block px-4 py-8 w-full text-right"
+              >
                 Collectibles
               </Link>
-              <Link href="/" className="hover:bg-orange-500 block px-4 py-8 w-full text-right">
+              <Link
+                href="/"
+                className="hover:bg-orange-500 block px-4 py-8 w-full text-right"
+              >
                 Media
               </Link>
-              <Link href="/" className="hover:bg-orange-500 block px-4 py-8 w-full text-right">
-                BUBs
+              <Link
+                href="/"
+                className="hover:bg-orange-500 block px-4 py-8 w-full text-right"
+              >
+                Impact
               </Link>
-              <Link href="/" className="hover:bg-orange-500 block px-4 py-8 w-full text-right">
+              <Link
+                href="/"
+                className="hover:bg-orange-500 block px-4 py-8 w-full text-right"
+              >
                 Shop
               </Link>
             </div>
