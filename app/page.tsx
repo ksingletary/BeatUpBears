5dd85fd164c649fa6fb2b510996e7e770de1c316
ksingletary <keithsingletary@gmail.com>
2024-05-19 23:50:59 -0400

    home page icons, other edits

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
 import Image from "next/image";
+import Link from "next/link";
+import { SiOpensea} from "react-icons/si";
+import { TbBrandCoinbase } from "react-icons/tb";
 
 export default function Home() {
   return (
     <>
-      <div className="w-screen h-auto relative flex">
-        <div className="min-h-[750px] w-full bg-yellow-50 ">
-          <Image src='/redpanda.png' width={700} height={700} alt="red panda pic" className="right-0 absolute"/>
+      <div className="w-screen h-auto relative flex flex-row-reverse items-center bg-blue-200 min-h-[750px]">
+        <Image src="/redpanda.png" width={700} height={700} alt="red panda pic" className=""/>
+        <div className=" ml-44 text-blackLight text-center space-y-6">
+          <h1 className="text-7xl font-apercuBold">
+            Beat Up But Okay
+          </h1>
+          <p className="text-2xl font-apercuRegular mb-4">
+            Vitalizing the web3 space by leveraging blockchain to build a global community 
+          </p>
+          <div className="flex justify-center space-x-6">
+            <Link href="#" className="text-blue-500">
+              <SiOpensea className="w-8 h-8" />
+            </Link>
+            <Link href="#" className=" text-indigo-700">
+              <Image src="/solana-sol-logo.svg" alt="solana logo" width={100} height={100} className="w-8 h-8"/>
+            </Link>
+            <Link href="#">
+              <TbBrandCoinbase className="w-8 h-8 text-blue-700" />
+            </Link>
+          </div>
         </div>
       </div>
     </>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,8 +26,27 @@ export function Navbar() {
         </button>
        
         {open && (
-          <div className="absolute opacity-65 bottom-16 left-0 ml-32 top-0 h-full w-48 p-4 bg-blackLight text-white">
-            <p>Additional Content</p>
+          <div className="fixed left-36 outline outline-black top-0 h-full w-[calc(33.000%-60px)] p-0 bg-blackLight font-CaeciliaSemi text-white bg-opacity-75 backdrop-blur-md">
+            <div className="flex flex-col justify-center h-full text-2xl -space-y-5 -mt-14 ">
+              <Link href="/about" className="hover:bg-orange-500 block px-4 py-8 w-full text-right">
+                About
+              </Link>
+              <Link href="/" className="hover:bg-orange-500 block px-4 py-8 w-full text-right">
+                Archives
+              </Link>
+              <Link href="/" className="hover:bg-orange-500 block px-4 py-8 w-full text-right">
+                Collectibles
+              </Link>
+              <Link href="/" className="hover:bg-orange-500 block px-4 py-8 w-full text-right">
+                Media
+              </Link>
+              <Link href="/" className="hover:bg-orange-500 block px-4 py-8 w-full text-right">
+                BUBs
+              </Link>
+              <Link href="/" className="hover:bg-orange-500 block px-4 py-8 w-full text-right">
+                Shop
+              </Link>
+            </div>
           </div>
         )}
       </div>
